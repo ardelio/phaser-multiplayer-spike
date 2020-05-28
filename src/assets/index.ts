aac54f9815ba65dd0c78b900d9b2cca591b54ed6
Anthony Sceresini <asceresini@gmail.com>
2020-05-28 23:29:24 +1000

    Setup Basic Phaser3 game
    From 'Making your first Phaser 3 game' @ http://phaser.io/tutorials/making-your-first-phaser-3-game/part1

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export const GROUND = require('./ground.png');
+export const LOGO = require('./logo.png');
+export const PLAYER = require('./player.png');
+export const SKY = require('./sky.png');
+export const STAR = require('./star.png');
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,107 @@
-import Phaser from 'phaser';
+import Phaser, { Physics, GameObjects } from 'phaser';
 
-const ASSETS = {
-  LOGO: require('./assets/logo.png') as string,
-};
+import * as ASSETS from './assets'
 
 export default class Demo extends Phaser.Scene {
+  private platforms: Phaser.Physics.Arcade.StaticGroup;
+  private player: Phaser.Physics.Arcade.Sprite;
+  private stars: Phaser.Physics.Arcade.Group;
+  private score: number;
+  private scoreText: GameObjects.Text;
+
   constructor() {
     super('demo');
+    this.score = 0;
   }
 
   preload() {
-    this.load.image('logo', ASSETS.LOGO);
+    this.load.image('ground', ASSETS.GROUND);
+    this.load.spritesheet('player', ASSETS.PLAYER, { frameWidth: 32, frameHeight: 48 });
+    this.load.image('star', ASSETS.STAR);
+    this.load.image('sky', ASSETS.SKY);
   }
 
   create() {
-    this.add.image(400, 440, 'logo');
+    this.platforms = this.physics.add.staticGroup()
+    this.add.image(400, 300, 'sky');
+    this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    this.player = this.physics.add.sprite(100, 450, 'player');
+
+    const { platforms, player } = this;
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, platforms);
+
+    this.stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    const { stars } = this;
+
+    stars.children.iterate(child => {
+        (child.body as Phaser.Physics.Arcade.Body).setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(stars, platforms);
+
+    this.physics.add.overlap(player, stars, this.collectStar, null, this);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'player', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
   }
 
-  update() {}
+  update() {
+    const { player } = this;
+    const cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+      player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(160);
+      player.anims.play('right', true);
+    } else {
+      player.setVelocityX(0);
+      player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+      player.setVelocityY(-330);
+    }
+  }
+
+  collectStar(player: Physics.Arcade.Sprite, star: Physics.Arcade.Image) {
+    star.disableBody(true, true);
+
+    this.score += 10;
+    this.scoreText.setText('Score: ' + this.score);
+  }
 }
 
 const config : Phaser.Types.Core.GameConfig = {
@@ -25,6 +109,13 @@ const config : Phaser.Types.Core.GameConfig = {
   parent: 'phaser-game-canvas',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: Demo,
 };
 
